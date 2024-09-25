72908379009658e80132de0c1dca712359d104f3
FrankCairampoma <ingfralch@gmail.com>
2024-09-25 17:52:40 -0500

    recuperar clave diseño movil

--- src/views/RecuperarClave.vue
+++ src/views/RecuperarClave.vue
@@ -4,43 +4,46 @@
       class="flex-1 bg-[url('@/assets/login/triangulos-login.svg')] bg-repeat-y bg-left-top flex flex-col justify-center h-screen"
     >
       <div
-        class="bg-[url('@/assets/recuperar.jpg')] bg-no-repeat bg-cover p-10 flex flex-col items-center justify-center h-[80vh]"
+        class="bg-[url('@/assets/recuperar.jpg')] bg-no-repeat bg-cover p-10 flex flex-col items-center justify-center sm:h-[80vh] h-[100vh]"
         id="recuperar"
       >
         <!-- Ajustes para el logo -->
-        <img src="../assets/logo-blanco.png" alt="" class="self-start w-60" />
+        <img
+          src="../assets/logo-blanco.png"
+          alt=""
+          class="sm:self-start sm:w-64 w-48 self-center sm:mt-5 mt-10"
+        />
         <form @submit.prevent="handleSubmit" class="m-auto">
           <div class="flex flex-col">
             <h3
               key="title"
-              class="text-3xl font-bold text-center text-white mb-10"
+              class="text-3xl sm:font-bold text-center text-white mb-10"
             >
               Recuperar clave web
             </h3>
             <div class="mt-4">
               <label
                 for="nueva_clave"
-                class="bg-credi-green text-white px-6 py-3 rounded-md focus:outline-none inline-block md:w-36 md:px-4 md:py-2 w-20 sm:px-2 sm:py-1"
+                class="text-white px-4 py-3 rounded-md focus:outline-none inline-block md:w-36 md:px-6 md:py-2"
               >
                 Nueva clave
               </label>
               <input
                 type="text"
-                class="bg-gray-200 px-6 py-3 rounded-md focus:outline-none md:ml-8 ml-2 md:px-4 md:py-2"
+                class="bg-gray-200 px-6 py-3 rounded-md focus:outline-none md:ml-8 ml-2 md:px-4 md:py-2 w-48"
                 ref="nueva_clave"
               />
             </div>
             <div class="mt-4 relative">
               <label
                 for="repetir_clave"
-                class="bg-credi-green text-white px-4 py-3 rounded-md focus:outline-none inline-block md:w-36 md:px-6 md:py-2"
+                class="text-white px-4 py-3 rounded-md focus:outline-none inline-block md:w-36 md:px-6 md:py-2"
               >
                 Repetir clave
               </label>
               <input
-                class="input bg-gray-200 px-6 py-3 rounded-md focus:outline-none md:ml-8 ml-2 md:px-4 md:py-2"
+                class="bg-gray-200 px-6 py-3 rounded-md focus:outline-none md:ml-8 ml-2 md:px-4 md:py-2 w-48"
                 ref="repetir_clave"
-                readonly
               />
             </div>
             <label class="text-red-500">Error message</label>
@@ -51,7 +54,7 @@
             ></div>
             <button
               key="login"
-              class="px-6 py-3 rounded-md focus:outline-none ml-8 m-10 bg-credi-blue text-white hover:bg-credi-blue transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"
+              class="px-6 py-3 rounded-md focus:outline-none ml-8 m-10 sm:bg-credi-blue bg-credi-green text-white hover:bg-credi-blue transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"
             >
               Confirmar
             </button>
